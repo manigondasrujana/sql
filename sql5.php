a4873a4ea1e95b77d16e81e136d78f192c8b6fce
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-11-03 16:32:51 -0400

    rows

--- sql5.php
+++ sql5.php
@@ -3,7 +3,7 @@
 $link = mysql_connect("sql1.njit.edu", "sm2555", "tzw8bjLL");
 mysql_select_db("sm2555", $link);
 
-$result = mysql_query("SELECT * FROM accounts", $link);
+$result = mysql_query("SELECT * FROM accounts where id<6", $link);
 $num_rows = mysql_num_rows($result);
 
 echo "$num_rows Rows\n";
